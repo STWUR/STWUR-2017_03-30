b1fed7794b007a55f9ce6145fb2550ac8b82ed42
psobczyk <Piotr.Sobczyk@infermedica.com>
2017-03-12 15:55:22 +0100

    poprawienie przetworzenia danych. Dodanie annotation ze źródłem danych

--- tworzenie_mapy_wyksztalcenie_wyzsze_w_wojewodztwach.R
+++ tworzenie_mapy_wyksztalcenie_wyzsze_w_wojewodztwach.R
@@ -11,6 +11,7 @@ load("data/ksztalt_wojewodztw_data_frame.Rdata")
 edu <- read.csv("data/education_data.csv")
 
 edu %>% 
+  filter(rok == 2015) %>%
   group_by(wojewodztwo) %>%
   summarise(procent_wyzsze_wyksztalcenie = sum(waga[edukacja == 'wyższe i policealne'])/sum(waga)) %>%
   mutate(wojewodztwo = tolower(wojewodztwo)) %>% 
@@ -25,11 +26,13 @@ p <- ggplot(data = plotData, mapping = aes(x = long, y = lat)) +
   scale_fill_distiller("%", palette = "YlGn", breaks = pretty_breaks(n = 6),
                        trans = "reverse", labels = percent) +
   guides(fill = guide_legend(reverse = TRUE)) +
-  ggtitle(label = "Procent osób z wyższym wykształceniem", subtitle = "W podziale na województwa") + 
+  ggtitle(label = "Procent osób z wyższym wykształceniem", subtitle = "W podziale na województwa w 2015") + 
   theme_map(base_size = 18) +
-  theme(plot.title=element_text(size=24, hjust = 0.5),
-        plot.subtitle=element_text(size=22, hjust = 0.5),
-        legend.position="right")
+  theme(plot.title=element_text(size=24, hjust = 0.5, family = "mono"),
+        plot.subtitle=element_text(size=22, hjust = 0.5, family = "mono"),
+        legend.position="right",
+        legend.key.height = unit(3, "cm"),
+        legend.key.width = unit(1.5, "cm"))
 p
 
 #### dodanie etykietet z procentami ####
@@ -39,12 +42,19 @@ library(ggrepel)
 #laczymy centroidy wojewodztw z danymi
 plotData_centroidy <- inner_join(centroidy_wojewodztw, edu_podsumowanie, by = "id")
 
-p +  
+p2 <- p +  
   geom_label(data=plotData_centroidy, 
              mapping = aes(x=x, y=y, 
                            label=paste0(formatC(100*procent_wyzsze_wyksztalcenie, 2), "%")), 
-             show.legend = FALSE) 
+             show.legend = FALSE) +
+  annotate("text", x = -Inf, y = -Inf, label = "Dane: Diagnoza Społeczna",
+           hjust=-0.1, vjust=-1, col="black", cex=6,
+           fontface = "plain", alpha = 0.2)
 
+# ggsave("wyksztalcenie_wojewodztwa_diagnoza.png", p2, width=10, height = 6)
+
+#' wyniki naszej estymacji NIE są do końca zgodne z szacunkami GUSu
+#' https://bdl.stat.gov.pl/BDL/dane/podgrup/temat/4/380/3435
 
 #' TODO
 #' Jak określać width i height, żeby kształt Polski był zgodny z wytycznymi GUS?
